a907db59b6ecde718df42ae04701bad247b17564
Nicolas Mathey <nicolas.mathey@etu.univ-nantes.fr>
2022-01-18 10:51:34 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("helloworld")
+from pycsp3 import *
+
+n = data # number of queens, it’s a data
+# q[i] is the column where is put the ith queen (at row i)
+q = VarArray(size=n, dom=range(n))
+satisfy(
+AllDifferent(q), # no 2 queens on the same column
+# no two queens on the same upward diagonal
+AllDifferent(q[i] + i for i in range(n)),
+# no two queens on the same downward diagonal
+AllDifferent(q[i] - i for i in range(n)))
+
