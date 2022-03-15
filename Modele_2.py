140473aaa8e6c4bfa98aa8db472686fce21c4250
Nicolas Mathey <nicolas.mathey@etu.univ-nantes.fr>
2022-03-15 10:50:58 +0100

    Modèle 1 et 2 finis

--- Modele_2.py
+++ Modele_2.py
@@ -7,14 +7,15 @@ graph = parser.parser(data[0])
 n = len(graph)
 
 f = VarArray(size=n, dom=range(1, n+1))
-liste_couples = [(i,j) for i in range(1,n+1) for j in range(1,n+1)]
 possible = []
-for u in graph.keys():
-    for v in graph[u]:
+for u in range(1,n+1):
+    for v in range(1,n+1):
         if (u != v) and min(abs(v - u), n - abs(v - u)) <= k:
             possible.append((u, v))
-print(possible)
+print("possible : ", possible)
 satisfy(
     AllDifferent(f),
-    ((f[u-1],f[v-1]) in possible for (u,v) in liste_couples)
+    [(f[u-1],f[v-1]) in possible for u in graph.keys() for v in graph[u]]
 )
+
+#python Modele_2.py -data=[Instances/didactic.mtx.rnd,10] -solve
---
+++ Modele_3.py
@@ -0,0 +1,6 @@
+from pycsp3 import *
+import parser
+
+[file,k] = data
+graph = parser.parser(data[0])
+
