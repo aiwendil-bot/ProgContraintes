ca21a376e756cc8003f8e6548f931c2e254339ef
adrien <adriencallico@hotmail.fr>
2022-04-03 14:39:53 +0200

    correction borne inf dans la recherche dico

--- recherche_k_dicho.py
+++ recherche_k_dicho.py
@@ -1,16 +1,17 @@
 from parser import parser
 from Modele_2 import satisfiabilite_model2
+from math import *
 
 
 def recherche_dicho(filename,time):
     graph = parser(filename)
     degrees = [len(k) for k in graph.values()]
-    inf = int(max(degrees) / 2)
+    inf = ceil(max(degrees) / 2)
     sup = int(len(graph) / 2)
     k = int(len(graph) / 4)
 
     while sup - inf > 2:
-        print(inf,sup)
+        print(inf, sup)
         if satisfiabilite_model2(filename, k,time):
             sup = k
         else:
